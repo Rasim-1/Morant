a2812eefb2e8d62625cfe9e3a7b780b096577edf
rasim <rasimhusanbaev20@gmail.com>
2025-03-09 16:43:52 +0500

    final

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import App from './App.jsx';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
-import { AppContextProvider } from './context/context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
